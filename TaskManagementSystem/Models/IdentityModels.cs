47b4cd22837c6821f814fdf5a27d5f64823bcdfb
Krusha Patel <krushampatel@yahoo.com>
2021-05-23 18:17:50 -0500

    add project class and projectTask class and add new migration

--- TaskManagementSystem/Models/IdentityModels.cs
+++ TaskManagementSystem/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,15 @@ namespace TaskManagementSystem.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<Project> Projects { get; set; }
+        public virtual ICollection<ProjectTask> ProjectTasks { get; set; }
+
+        public ApplicationUser()
+        {
+            this.Projects = new HashSet<Project>();
+            this.ProjectTasks = new HashSet<ProjectTask>();
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +30,9 @@ namespace TaskManagementSystem.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectTask> ProjectTasks { get; set; }
+
         public ApplicationDbContext()
             : base("TaskConnectionString", throwIfV1Schema: false)
         {
---
+++ TaskManagementSystem/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementSystem.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        public virtual ICollection<ProjectTask> ProjectTasks { get; set; }
+
+        public Project()
+        {
+            this.ProjectTasks = new HashSet<ProjectTask>();
+        }
+    }
+}
