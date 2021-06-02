6e8f820a4ad08dd0d09414fc00261199d7a333d2
Krusha Patel <krushampatel@yahoo.com>
2021-06-02 16:55:04 -0500

    organized controller methods and views

--- TaskManagementSystem/Controllers/ProjectTasksController.cs
+++ TaskManagementSystem/Controllers/ProjectTasksController.cs
@@ -16,14 +16,11 @@ namespace TaskManagementSystem.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: ProjectTasks
         public ActionResult Index()
         {
-            var projectTasks = db.ProjectTasks.Include(p => p.ApplicationUser).Include(p => p.Project);
-            return View(projectTasks.ToList());
+            return RedirectToAction("AllTasks", "ProjectTasks");
         }
 
-        // GET: ProjectTasks/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -38,7 +35,6 @@ namespace TaskManagementSystem.Controllers
             return View(projectTask);
         }
 
-        // GET: ProjectTasks/Create
         public ActionResult Create()
         {
             ViewBag.ApplicationUserId = new SelectList(db.Users, "Id", "Email");
@@ -46,34 +42,26 @@ namespace TaskManagementSystem.Controllers
             return View();
         }
 
-        // POST: ProjectTasks/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Name,CompletionPercentage,IsCompleted,ApplicationUserId,ProjectId")] ProjectTask projectTask)
         {
             if (ModelState.IsValid)
             {
-                /*db.ProjectTasks.Add(projectTask);
-                db.SaveChanges();*/
                 ProjectTaskHelper.AddProjectTask(projectTask);
                 return RedirectToAction("Index");
             }
-
             ViewBag.ApplicationUserId = new SelectList(db.Users, "Id", "Email", projectTask.ApplicationUserId);
             ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Name", projectTask.ProjectId);
             return View(projectTask);
         }
 
-        // GET: ProjectTasks/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            /*ProjectTask projectTask = db.ProjectTasks.Find(id);*/
             ProjectTask projectTask = ProjectTaskHelper.FindProjectTask(id);
             if (projectTask == null)
             {
@@ -84,9 +72,6 @@ namespace TaskManagementSystem.Controllers
             return View(projectTask);
         }
 
-        // POST: ProjectTasks/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Name,CompletionPercentage,IsCompleted,ApplicationUserId,ProjectId")] ProjectTask projectTask)
@@ -102,14 +87,12 @@ namespace TaskManagementSystem.Controllers
             return View(projectTask);
         }
 
-        // GET: ProjectTasks/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            /*ProjectTask projectTask = db.ProjectTasks.Find(id);*/
             ProjectTask projectTask = ProjectTaskHelper.FindProjectTask(id);
             if (projectTask == null)
             {
@@ -118,14 +101,10 @@ namespace TaskManagementSystem.Controllers
             return View(projectTask);
         }
 
-        // POST: ProjectTasks/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            //ProjectTask projectTask = db.ProjectTasks.Find(id);
-            //db.ProjectTasks.Remove(projectTask);
-            //db.SaveChanges();
             ProjectTaskHelper.DeleteProjectTask(id);
             return RedirectToAction("Index");
         }
